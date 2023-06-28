b64e9f5281095cf5e7784e629dbde67622142d3a
hunaidbaig <127725548+hunaidbaig@users.noreply.github.com>
2023-06-28 23:56:56 +0500

    Update RatingRepository.java

--- src/main/java/com/rating/rating/repository/RatingRepository.java
+++ src/main/java/com/rating/rating/repository/RatingRepository.java
@@ -2,6 +2,8 @@ package com.rating.rating.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import com.rating.rating.model.Rating;
 
 public interface RatingRepository extends JpaRepository<Rating,Long> {
